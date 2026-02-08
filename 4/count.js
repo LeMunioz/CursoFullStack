043cd06482eebfea15efb79c675b35c8083ca452
LeMunioz <angel.munoz5884@alumnos.udg.mx>
2026-02-07 21:50:04 -0600

    [contador y scope: imp] -> ejercisios del modulo de fundamentos de javascript

---
+++ 4/count.js
@@ -0,0 +1,19 @@
+let contador = 0; 
+
+function increaseCount() {
+  contador++; 
+  muestraContador();
+  checkCountValue();
+}
+function muestraContador() {
+document.getElementById('countDisplay').innerHTML=contador; 
+}
+
+function checaValorContador() {
+  if (contador === 10) {
+    alert("haz superado los 10 seguidores en tinder YEIIII 😊");
+  } else if (contador === 20) {
+    alert("Nomanches, ya tienes mas de 20!!!");
+  }
+}
+
---
+++ 4/scope.js
@@ -0,0 +1,17 @@
+// alcance global
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// alcance de bloque
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+
+// Alcance global
+console.log(globalVar);
+console.log(globalLet); 
+console.log(globalConst); 
